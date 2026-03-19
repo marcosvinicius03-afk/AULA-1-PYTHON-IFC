dfc4e322c13126c7ec0c6e048c6e729a0346d9db
marcosvinicius03-afk <marcosvvinicius03@gmail.com>
2026-03-19 09:56:47 -0300

    Lista de exercicios feitos em aula

---
+++ Lista de exercícios Difícies/7.py
@@ -0,0 +1,13 @@
+# 7. Construir um algoritmo para ler o raio de uma circunferência e mostrar o perímetro ( 2 x π x raio ) e a área ( π x raio2). Utilize o π como constante.
+
+import math
+
+# Entrada
+raio = float(input("Quanto vale o raio: "))
+
+# Processamento
+perimetro = (2*math.pi*raio)
+area (math.pi*(raio**2))
+
+# Saida
+print(f"O perimetro vale {perimetro:.2f} e a area {area:.2f}")
