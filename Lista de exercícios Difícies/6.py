889f19334f19ed19f9fa6e78853d472ed3cc1c91
marcosvinicius03-afk <marcosvvinicius03@gmail.com>
2026-03-19 10:10:53 -0300

    exercicio 6 adicionado

---
+++ Lista de exercícios Difícies/6.py
@@ -0,0 +1,11 @@
+# 6. Criar um algoritmo para ler a base e a altura de um triângulo e mostrar a sua área ( ( base x altura ) / 2 ).
+
+# Entrada
+base = float(input("valor da base: "))
+altura = float(input("valor da altura: "))
+
+# Processamento
+area = ((base*altura)/2)
+
+# Saida
+print(f"A area vale {area:.2f}")
